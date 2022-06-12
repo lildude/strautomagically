900aaecb572f1a7c063a3d877d0a73c35f8c5699
Colin Seymour <colin@symr.io>
2022-06-12 11:22:18 +0100

    Improve callback

--- callback.go
+++ callback.go
@@ -2,38 +2,33 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
-	"log"
 	"net/http"
+	"os"
+	"strings"
 )
 
-const challengeKey = "hub.challenge"
-
-type callbackResponse struct {
-	Challenge string `json:"hub.challenge"`
-}
-
 func callbackHandler(w http.ResponseWriter, r *http.Request) {
-	challenge := r.URL.Query().Get(challengeKey)
-	if challenge == "" {
+	q := r.URL.Query()
+	challenge, ok := q["hub.challenge"]
+	if !ok {
 		w.WriteHeader(http.StatusBadRequest)
-		if _, err := w.Write([]byte(fmt.Sprintf("missing query param: %s\n", challengeKey))); err != nil {
-			log.Println(err)
-		}
+		w.Write([]byte("missing query param: hub.challenge")) //nolint:errcheck
 		return
 	}
-	resp, err := json.Marshal(callbackResponse{
-		Challenge: challenge,
-	})
-	if err != nil {
-		w.WriteHeader(http.StatusServiceUnavailable)
-		if _, err = w.Write([]byte(fmt.Sprintf("%s\n", err))); err != nil {
-			log.Println(err)
-		}
+	verify, ok := q["hub.verify_token"]
+	if !ok {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("missing query param: hub.verify_token")) //nolint:errcheck
 		return
 	}
-	w.WriteHeader(http.StatusOK)
-	if _, err = w.Write([]byte(fmt.Sprintf("%s\n", resp))); err != nil {
-		log.Println(err)
+	if strings.Join(verify, "") != os.Getenv("STRAVA_VERIFY_TOKEN") { // TODO: generate this
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("verify token mismatch")) //nolint:errcheck
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	if err := json.NewEncoder(w).Encode(map[string]string{"hub.challenge": challenge[0]}); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
 	}
 }
--- callback_test.go
+++ callback_test.go
@@ -8,7 +8,7 @@ import (
 )
 
 func TestSuccessfulCallback(t *testing.T) {
-	req := httptest.NewRequest(http.MethodGet, "/?hub.mode=subscribe&hub.challenge=mychallenge", nil)
+	req := httptest.NewRequest(http.MethodGet, "/?hub.mode=subscribe&hub.challenge=mychallenge&hub.verify_token=mytoken", nil)
 	w := httptest.NewRecorder()
 	callbackHandler(w, req)
 	res := w.Result()
@@ -43,7 +43,49 @@ func TestMissingChallenge(t *testing.T) {
 		t.Errorf("expected status to be %d got %d", http.StatusBadRequest, res.StatusCode)
 	}
 
-	expected := "missing query param: hub.challenge\n"
+	expected := "missing query param: hub.challenge"
+	if string(data) != expected {
+		t.Errorf("expected '%s' got '%v'", expected, string(data))
+	}
+}
+
+func TestMissingVerifyToken(t *testing.T) {
+	req := httptest.NewRequest(http.MethodGet, "/?hub.mode=subscribe&hub.challenge=challenge", nil)
+	w := httptest.NewRecorder()
+	callbackHandler(w, req)
+	res := w.Result()
+	defer res.Body.Close()
+	data, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		t.Errorf("expected error to be nil got %v", err)
+	}
+
+	if res.StatusCode != http.StatusBadRequest {
+		t.Errorf("expected status to be %d got %d", http.StatusBadRequest, res.StatusCode)
+	}
+
+	expected := "missing query param: hub.verify_token"
+	if string(data) != expected {
+		t.Errorf("expected '%s' got '%v'", expected, string(data))
+	}
+}
+
+func TestIncorrectVerifyToken(t *testing.T) {
+	req := httptest.NewRequest(http.MethodGet, "/?hub.mode=subscribe&hub.challenge=challenge&hub.verify_token=wrong", nil)
+	w := httptest.NewRecorder()
+	callbackHandler(w, req)
+	res := w.Result()
+	defer res.Body.Close()
+	data, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		t.Errorf("expected error to be nil got %v", err)
+	}
+
+	if res.StatusCode != http.StatusBadRequest {
+		t.Errorf("expected status to be %d got %d", http.StatusBadRequest, res.StatusCode)
+	}
+
+	expected := "verify token mismatch"
 	if string(data) != expected {
 		t.Errorf("expected '%s' got '%v'", expected, string(data))
 	}
