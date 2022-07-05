8e59c8b5ca6804ebf945bab0824352f859328d5b
Colin Seymour <colin@symr.io>
2022-07-05 18:19:52 +0100

    Add Get and Set JSON methods to cache

--- internal/cache/cache.go
+++ internal/cache/cache.go
@@ -2,6 +2,7 @@ package cache
 
 import (
 	"context"
+	"encoding/json"
 
 	"github.com/go-redis/redis/v8"
 )
@@ -9,6 +10,8 @@ import (
 type Cache interface {
 	Get(key string) (interface{}, error)
 	Set(key string, value interface{}) error
+	GetJSON(key string, v interface{}) error
+	SetJSON(key string, value interface{}) error
 }
 
 type RedisCache struct {
@@ -35,14 +38,38 @@ func NewRedisCache(addr string) (Cache, error) {
 	}, nil
 }
 
+// Set stores a value in the cache
 func (rc *RedisCache) Set(key string, value interface{}) error {
 	return rc.conn.Set(rc.ctx, key, value, 0).Err()
 }
 
+// Get retrieves a value from the cache
 func (rc *RedisCache) Get(key string) (interface{}, error) {
 	if value, err := rc.conn.Get(rc.ctx, key).Result(); err == nil || err == redis.Nil {
 		return value, nil
 	} else {
 		return nil, err
 	}
+}
+
+// GetJSON retrieves a JSON string and unmarshals it into the given interface
+func (rc *RedisCache) GetJSON(key string, value interface{}) error {
+	v, err := rc.Get(key)
+	if err != nil {
+		return err
+	}
+
+	if err := json.Unmarshal([]byte(v.(string)), &value); err != nil {
+		return err
+	}
+	return nil
+}
+
+// SetJSON stores a struct as a JSON string
+func (rc *RedisCache) SetJSON(key string, value interface{}) error {
+	t, err := json.Marshal(value)
+	if err != nil {
+		return err
+	}
+	return rc.Set(key, string(t))
 }
