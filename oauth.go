e5e871abd840b2dc0a8bc9353662f582cc908992
Colin Seymour <colin@symr.io>
2022-07-26 15:58:07 +0100

    Start adding oauth tests

--- oauth.go
+++ oauth.go
@@ -27,13 +27,10 @@ func authHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 		return
 	}
+
 	authToken := &oauth2.Token{}
-	err = cache.GetJSON("strava_auth_token", &authToken)
-	if err != nil {
-		log.Printf("unable to get token: %s", err)
-		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-		return
-	}
+	cache.GetJSON("strava_auth_token", &authToken) //nolint:errcheck
+
 	if state == "" {
 		if authToken.AccessToken == "" {
 			u := strava.OauthConfig.AuthCodeURL(stateToken)
@@ -45,21 +42,25 @@ func authHandler(w http.ResponseWriter, r *http.Request) {
 	} else {
 		if state != stateToken {
 			http.Error(w, "state invalid", http.StatusBadRequest)
+			return
 		}
 		code := r.Form.Get("code")
 		if code == "" {
 			http.Error(w, "code not found", http.StatusBadRequest)
+			return
 		}
 		token, err := strava.OauthConfig.Exchange(context.Background(), code)
 		if err != nil {
 			log.Println(err)
 			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			return
 		}
 
 		athlete, ok := token.Extra("athlete").(map[string]interface{})
 		if !ok {
 			log.Println("unable to get athete info", err)
 			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			return
 		}
 
 		err = cache.SetJSON("strava_auth_token", token)
---
+++ oauth_test.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/alicebob/miniredis/v2"
+	"github.com/jarcoal/httpmock"
+)
+
+func TestAuthHandler(t *testing.T) {
+	// Discard logs to avoid polluting test output
+	// log.SetOutput(ioutil.Discard)
+
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	oat := `{"access_token":"123456789","token_type":"Bearer","refresh_token":"987654321","expiry":"2022-07-12T18:30:36.917400827Z", "athlete":{"id":1,"username":"test"}}`
+
+	httpmock.RegisterResponder("POST", "https://www.strava.com/oauth/token",
+		httpmock.NewStringResponder(200, oat))
+
+	httpmock.RegisterResponder("GET", "https://www.strava.com/api/v3/push_subscriptions",
+		httpmock.NewStringResponder(200, `{}`))
+
+	httpmock.RegisterResponder("POST", "https://www.strava.com/api/v3/push_subscriptions",
+		httpmock.NewStringResponder(200, `{"id":1}`))
+
+	r := miniredis.RunT(t)
+	defer r.Close()
+	os.Setenv("REDIS_URL", fmt.Sprintf("redis://%s", r.Addr()))
+	os.Setenv("STATE_TOKEN", "test-state-token")
+
+	tests := []struct {
+		name  string
+		query string
+		body  string
+		want  int
+	}{
+		{
+			"no state redirects to strava",
+			"",
+			"",
+			http.StatusFound,
+		},
+		{
+			"invalid state",
+			"?state=invalid-state",
+			"",
+			http.StatusBadRequest,
+		},
+		{
+			"valid state but no code",
+			"?state=test-state-token",
+			"",
+			http.StatusBadRequest,
+		},
+		{
+			"valid state and code",
+			"?state=test-state-token&code=test-code",
+			"",
+			http.StatusFound,
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			req, err := http.NewRequest("POST", fmt.Sprintf("/auth%s", tc.query), strings.NewReader(tc.body))
+			if err != nil {
+				t.Fatal(err)
+			}
+			rr := httptest.NewRecorder()
+			handler := http.HandlerFunc(authHandler)
+			handler.ServeHTTP(rr, req)
+
+			if status := rr.Code; status != tc.want {
+				t.Errorf("%s: handler returned wrong status code: got %d want %d", tc.name, status, tc.want)
+			}
+		})
+	}
+}
