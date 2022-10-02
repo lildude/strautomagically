fddada355e02d87e0565a81a3802bbeb0fa1d493
Colin Seymour <colin@symr.io>
2022-10-02 14:03:05 +0100

    Refactor callback tests

--- internal/handlers/callback/callback_test.go
+++ internal/handlers/callback/callback_test.go
@@ -1,95 +1,71 @@
 package callback
 
 import (
-	"io/ioutil"
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"os"
 	"testing"
 )
 
-func TestSuccessfulCallback(t *testing.T) {
-	os.Setenv("STRAVA_VERIFY_TOKEN", "mytoken")
-	req := httptest.NewRequest(http.MethodGet, "/?hub.mode=subscribe&hub.challenge=mychallenge&hub.verify_token=mytoken", nil)
-	w := httptest.NewRecorder()
-	CallbackHandler(w, req)
-	res := w.Result()
-	defer res.Body.Close()
-	data, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		t.Errorf("expected error to be nil got %v", err)
+func TestCallbackHandler(t *testing.T) {
+	tests := []struct {
+		name        string
+		queryParams string
+		wantStatus  int
+	}{
+		{
+			"successful callback",
+			"hub.mode=subscribe&hub.challenge=mychallenge&hub.verify_token=mytoken",
+			http.StatusOK,
+		},
+		{
+			"missing query param: hub.challenge",
+			"hub.mode=subscribe&hub.verify_token=mytoken",
+			http.StatusBadRequest,
+		},
+		{
+			"missing query param: hub.verify_token",
+			"hub.mode=subscribe&hub.challenge=mychallenge",
+			http.StatusBadRequest,
+		},
+		{
+			"verify token mismatch",
+			"hub.mode=subscribe&hub.challenge=mychallenge&hub.verify_token=wrong",
+			http.StatusBadRequest,
+		},
 	}
 
-	if res.StatusCode != http.StatusOK {
-		t.Errorf("expected status to be %d got %d", http.StatusOK, res.StatusCode)
-	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			os.Setenv("STRAVA_VERIFY_TOKEN", "mytoken")
+			req := httptest.NewRequest(http.MethodGet, "/?"+tt.queryParams, nil)
+			w := httptest.NewRecorder()
+			CallbackHandler(w, req)
+			res := w.Result()
+			defer res.Body.Close()
+			data, err := io.ReadAll(res.Body)
+			if err != nil {
+				t.Errorf("expected error to be nil got %v", err)
+			}
 
-	expected := "{\"hub.challenge\":\"mychallenge\"}\n"
-	if string(data) != expected {
-		t.Errorf("expected '%s' got '%v'", expected, string(data))
-	}
-}
+			if res.StatusCode != tt.wantStatus {
+				t.Errorf("expected status to be %d got %d", tt.wantStatus, res.StatusCode)
+			}
 
-func TestMissingChallenge(t *testing.T) {
-	req := httptest.NewRequest(http.MethodGet, "/?hub.mode=subscribe", nil)
-	w := httptest.NewRecorder()
-	CallbackHandler(w, req)
-	res := w.Result()
-	defer res.Body.Close()
-	data, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		t.Errorf("expected error to be nil got %v", err)
-	}
+			if tt.wantStatus == http.StatusOK {
+				expected := "{\"hub.challenge\":\"mychallenge\"}\n"
+				if string(data) != expected {
+					t.Errorf("expected '%s' got '%v'", expected, string(data))
+				}
+			}
 
-	if res.StatusCode != http.StatusBadRequest {
-		t.Errorf("expected status to be %d got %d", http.StatusBadRequest, res.StatusCode)
-	}
-
-	expected := "missing query param: hub.challenge"
-	if string(data) != expected {
-		t.Errorf("expected '%s' got '%v'", expected, string(data))
-	}
-}
-
-func TestMissingVerifyToken(t *testing.T) {
-	req := httptest.NewRequest(http.MethodGet, "/?hub.mode=subscribe&hub.challenge=challenge", nil)
-	w := httptest.NewRecorder()
-	CallbackHandler(w, req)
-	res := w.Result()
-	defer res.Body.Close()
-	data, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		t.Errorf("expected error to be nil got %v", err)
-	}
-
-	if res.StatusCode != http.StatusBadRequest {
-		t.Errorf("expected status to be %d got %d", http.StatusBadRequest, res.StatusCode)
-	}
-
-	expected := "missing query param: hub.verify_token"
-	if string(data) != expected {
-		t.Errorf("expected '%s' got '%v'", expected, string(data))
-	}
-}
-
-func TestIncorrectVerifyToken(t *testing.T) {
-	os.Setenv("STRAVA_VERIFY_TOKEN", "mytoken")
-	req := httptest.NewRequest(http.MethodGet, "/?hub.mode=subscribe&hub.challenge=challenge&hub.verify_token=wrong", nil)
-	w := httptest.NewRecorder()
-	CallbackHandler(w, req)
-	res := w.Result()
-	defer res.Body.Close()
-	data, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		t.Errorf("expected error to be nil got %v", err)
-	}
-
-	if res.StatusCode != http.StatusBadRequest {
-		t.Errorf("expected status to be %d got %d", http.StatusBadRequest, res.StatusCode)
-	}
-
-	expected := "verify token mismatch"
-	if string(data) != expected {
-		t.Errorf("expected '%s' got '%v'", expected, string(data))
+			if tt.wantStatus == http.StatusBadRequest {
+				expected := tt.name
+				if string(data) != expected {
+					t.Errorf("expected '%s' got '%v'", expected, string(data))
+				}
+			}
+		})
 	}
 }
